31867670290acf6cd747a03516585577ef9e9a7f
JeffreedoM <jeffreedomtabz@gmail.com>
2026-02-26 11:41:15 +0800

    page builder collapsible

--- src/pages/admin/PageBuilder/PageBuilder.jsx
+++ src/pages/admin/PageBuilder/PageBuilder.jsx
@@ -9,24 +9,10 @@ import {
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { Separator } from "@/components/ui/separator";
-import { Card, CardTitle, CardContent, CardHeader } from "@/components/ui/card";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
+import { Accordion } from "radix-ui";
 
 // icons
-import { SquarePen } from "lucide-react";
+import { SquarePen, ChevronDown } from "lucide-react";
 
 export default function PageBuilder() {
   return (
@@ -35,74 +21,60 @@ export default function PageBuilder() {
       <div className="flex flex-1 flex-col">
         <div className="@container/main flex flex-1 flex-col gap-2">
           <div className="container mx-auto py-10">
-            <div className="max-w-4xl">
-              
-              <div className="rounded-md">
+            <Accordion.Root
+              className="max-w-4xl"
+              type="single"
+              collapsible
+              defaultValue="hero-section"
+            >
+              <Accordion.Item value="hero-section" className="rounded-md">
                 <div className="bg-accent flex items-center justify-between px-6 py-3">
                   <h2 className="text-lg font-semibold">Hero Section</h2>
-                  <div className="flex gap-2">
-                    <Button
-                      variant="outline"
-                      size="sm"
-                      className="cursor-pointer"
+                  <div className="flex items-center gap-2">
+                    <Accordion.Trigger
+                      className="data-[state=closed]:hidden"
+                      disabled
                     >
-                      <SquarePen className="h-4 w-4" />
-                      Edit
-                    </Button>
+                      <Button
+                        variant="outline"
+                        size="sm"
+                        className="cursor-pointer"
+                      >
+                        <SquarePen className="h-4 w-4" />
+                        Edit
+                      </Button>
+                    </Accordion.Trigger>
+                    <Accordion.Trigger className="transition-transform duration-300 data-[state=open]:rotate-180">
+                      <ChevronDown className="transform" aria-hidden />
+                    </Accordion.Trigger>
                   </div>
                 </div>
-                <FieldGroup className="max-w-xl px-6 pt-4 pb-10">
-                  <Field>
-                    <FieldLabel htmlFor="page-title">Page Title</FieldLabel>
-                    <Input id="page-title" placeholder="Enter page title" />
-                    <FieldDescription>
-                      Title of the page to be created.
-                    </FieldDescription>
-                  </Field>
+                <Accordion.Content>
+                  <FieldGroup className="max-w-xl px-6 pt-4 pb-10">
+                    <Field>
+                      <FieldLabel htmlFor="page-title">Page Title</FieldLabel>
+                      <Input id="page-title" placeholder="Enter page title" />
+                      <FieldDescription>
+                        Title of the page to be created.
+                      </FieldDescription>
+                    </Field>
 
-                  <Field>
-                    <FieldLabel>Description / Tagline</FieldLabel>
-                    <Textarea placeholder="Enter page description or tagline" />
-                    <FieldDescription>
-                      Description or tagline for the page.
-                    </FieldDescription>
-                  </Field>
+                    <Field>
+                      <FieldLabel>Description / Tagline</FieldLabel>
+                      <Textarea placeholder="Enter page description or tagline" />
+                      <FieldDescription>
+                        Description or tagline for the page.
+                      </FieldDescription>
+                    </Field>
 
-                  <Field>
-                    <FieldLabel>Image</FieldLabel>
-                    <Input type="file" accept="image/*" />
-                  </Field>
-                </FieldGroup>
-              </div>
-
-              <div className="rounded-md">
-                <div className="bg-accent flex items-center justify-between px-6 py-3">
-                  <h2 className="text-lg font-semibold">Discounts</h2>
-                </div>
-                <FieldGroup className="max-w-xl px-6 pt-4 pb-10">
-                  <Field>
-                    <FieldLabel htmlFor="page-title">Page Title</FieldLabel>
-                    <Input id="page-title" placeholder="Enter page title" />
-                    <FieldDescription>
-                      Title of the page to be created.
-                    </FieldDescription>
-                  </Field>
-
-                  <Field>
-                    <FieldLabel>Description / Tagline</FieldLabel>
-                    <Textarea placeholder="Enter page description or tagline" />
-                    <FieldDescription>
-                      Description or tagline for the page.
-                    </FieldDescription>
-                  </Field>
-
-                  <Field>
-                    <FieldLabel>Image</FieldLabel>
-                    <Input type="file" accept="image/*" />
-                  </Field>
-                </FieldGroup>
-              </div>
-            </div>
+                    <Field>
+                      <FieldLabel>Image</FieldLabel>
+                      <Input type="file" accept="image/*" />
+                    </Field>
+                  </FieldGroup>
+                </Accordion.Content>
+              </Accordion.Item>
+            </Accordion.Root>
           </div>
         </div>
       </div>
