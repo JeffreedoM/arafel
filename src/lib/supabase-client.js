fd84d79d162db4df06da6fea2ee4e8e4b8e4026d
JeffreedoM <jeffreedomtabz@gmail.com>
2026-05-24 22:29:07 +0800

    added checking for env

--- src/lib/supabase-client.js
+++ src/lib/supabase-client.js
@@ -3,4 +3,11 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+// Mag-add tayo ng checking para malaman natin kung nawawala ang env sa ibang page
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error(
+    "Babala: Nawawala ang Supabase Environment Variables sa page na ito!",
+  );
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
